c1e8febfc9b91706405af665e7134f4935ff33e3
bezo16 <bezo16@protonmail.ch>
2022-01-28 19:05:02 +0100

    .

--- kvbot.js
+++ kvbot.js
@@ -31,7 +31,7 @@ client.on('message',message => {
     sbHandler(message) 
     ccHandler(message)
 
-    if(message.content === '?purna') message.channel.send('prijemny mlady oddany a talentovany barber a večny vtipkar')
+    if(message.content === '?purna') message.channel.send('prijemny mlady oddany a talentovany barber a večny vtipKAR')
 
     kvEvents(message)
     custom(message)
