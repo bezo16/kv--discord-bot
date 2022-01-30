2599d825bd61eb4c901f6d6fa1c4e088716410a9
bezo16 <bezo16@protonmail.ch>
2022-01-30 19:47:13 +0100

    .

--- data/vedicMantras.js
+++ data/vedicMantras.js
@@ -4,7 +4,84 @@
         {
             trigger: '?pancha-tattva',
             text: 'śrī-kṛṣṇa-caitanya prabhu-nityānanda,śrī-advaita gadādhara śrīvāsādi-gaura-bhakta-vṛnda',
-        },   
+        },
+        {
+            trigger: '?maha-mantra',
+            text: 'text',
+        },    
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+        // {
+        //     trigger: '?',
+        //     text: 'text',
+        // }, 
+
     ]
 
 
--- handlers/vedicMantras.js
+++ handlers/vedicMantras.js
@@ -5,6 +5,15 @@ const mantras = require('../data/vedicMantras')
     function vedicMantras(message) {
         let content = message.content
 
+        if(content === '?mantras') {
+            let allMantras = ''
+            mantras.forEach((mantra,index) => {
+                if(mantras.length - 1 === index) allMantras += `${mantra.trigger}`
+                else allMantras += `${mantra.trigger},`
+            })
+            message.channel.send(allMantras)
+        } 
+
         mantras.forEach((mantra,index) => {
             if(content === mantra.trigger) message.channel.send(mantra.text)
         })
