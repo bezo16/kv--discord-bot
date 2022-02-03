014e961be73f128343343e5e8c3962eee5e4eab2
bezo16 <bezo16@protonmail.ch>
2022-02-03 21:09:40 +0100

    .

--- handlers/custom.js
+++ handlers/custom.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js')
+const sendImg = require('../functions/sendImageQuote')
 
 function custom(message) {
 
     if(message.content.split(' ')[0].toLowerCase() === 'customquote' && message.content.includes('"') && message.content.includes('{') && message.content.includes('}')) {
         let text = message.content.slice(message.content.indexOf('"') + 1,message.content.lastIndexOf('"'))
         let book = message.content.slice(message.content.indexOf('{') + 1,message.content.indexOf('}'))
-        sendImageQuote(message,`${text}`,`${book}`)
+        sendImg(message,`${text}`,`${book}`)
         setTimeout(() => {
             message.delete()
             }, 2000);
