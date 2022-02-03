d3b5422bee9b3802c0a36050ab9d7622c309f014
bezo16 <bezo16@protonmail.ch>
2022-02-03 21:16:32 +0100

    .

--- handlers/custom.js
+++ handlers/custom.js
@@ -5,10 +5,11 @@ function custom(message) {
     if(message.content.split(' ')[0].toLowerCase() === 'customquote' && message.content.includes('"') && message.content.includes('{') && message.content.includes('}')) {
         let text = message.content.slice(message.content.indexOf('"') + 1,message.content.lastIndexOf('"'))
         let book = message.content.slice(message.content.indexOf('{') + 1,message.content.indexOf('}'))
+        message.channel.send('workin')
         sendImg(message,`${text}`,`${book}`)
         setTimeout(() => {
             message.delete()
-            }, 2000);
+            }, 2500);
         }
 }
 
