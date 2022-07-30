bf2c4587d10444fdcb8e35b0457b7cdaa35ce338
bezo16 <bezo16@protonmail.ch>
2022-07-30 09:54:36 +0200

    fixed kv events

--- functions/sendEventsScreen.js
+++ functions/sendEventsScreen.js
@@ -4,16 +4,19 @@ const Canvas = require('canvas')
 const fs = require('fs')
 const fullPageScreenshot = require('../node_modules/fullpage-puppeteer-screenshot/index')
 
+const width = 2100
+const height = 800
+
 async function sendScreen(message) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
+  await page.setViewport({ width, height });
   await page.goto('https://kv-events.netlify.app/?st=1');
   await fullPageScreenshot(page, { path: './foo.png' });
   await browser.close();
 
   const screenshot = await Canvas.loadImage('./foo.png')
-  const canvas = Canvas.createCanvas(1920, 1400)
+  const canvas = Canvas.createCanvas(width, height)
   const ctx = canvas.getContext('2d')
   ctx.drawImage(screenshot, 0, 0, canvas.width, canvas.height)
   const atachment = new Discord.MessageAttachment(canvas.toBuffer(), 'screenshot.png')
--- functions/sendEventsScreenDate.js
+++ functions/sendEventsScreenDate.js
@@ -2,34 +2,26 @@ const Discord = require('discord.js')
 const puppeteer = require('puppeteer')
 const Canvas = require('canvas')
 const fs = require('fs')
+const fullPageScreenshot = require('../node_modules/fullpage-puppeteer-screenshot/index')
 
-const fullScreenshot = () => {}
+const width = 2100
+const height = 800
 
 async function sendScreenDate(message, date) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox']
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(`https://kv-events.netlify.app/?date=20${date.year}-${date.month}-01&st=1`, {
-    waitUntil: 'networkidle2'
-  });
-
-  await fullScreenshot(page, {
-    path: 'foo.png'
-  });
-
+  await page.setViewport({ width, height });
+  await page.goto(`https://kv-events.netlify.app/?date=20${date.year}-${date.month}-01&st=1`);
+  await fullPageScreenshot(page, { path: './foo.png' });
   await browser.close();
 
   const screenshot = await Canvas.loadImage('./foo.png')
-  const canvas = Canvas.createCanvas(1200, 1000)
+  const canvas = Canvas.createCanvas(width, height)
   const ctx = canvas.getContext('2d')
   ctx.drawImage(screenshot, 0, 0, canvas.width, canvas.height)
   const atachment = new Discord.MessageAttachment(canvas.toBuffer(), 'kv-events.png')
   message.channel.send({ files: [atachment] })
-  console.warn('send img')
   fs.unlink('./foo.png', () => {})
-  console.warn('dele file')
 }
 
 module.exports = sendScreenDate
