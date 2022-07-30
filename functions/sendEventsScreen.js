629ac689ecfedcc2d67d91bec4ff94529e0f7b34
bezo16 <bezo16@protonmail.ch>
2022-07-30 10:01:27 +0200

    updated pupeteer launch

--- functions/sendEventsScreen.js
+++ functions/sendEventsScreen.js
@@ -8,7 +8,7 @@ const width = 2100
 const height = 800
 
 async function sendScreen(message) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.setViewport({ width, height });
   await page.goto('https://kv-events.netlify.app/?st=1');
--- functions/sendEventsScreenDate.js
+++ functions/sendEventsScreenDate.js
@@ -8,7 +8,7 @@ const width = 2100
 const height = 800
 
 async function sendScreenDate(message, date) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.setViewport({ width, height });
   await page.goto(`https://kv-events.netlify.app/?date=20${date.year}-${date.month}-01&st=1`);
