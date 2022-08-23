e3bd57aa1195519126eab0dec2ced685c6a55402
bezo16 <jakub@buff.game>
2022-08-23 10:45:22 +0200

    updated facebook daily quotes from .png to .jpg

--- handlers/facebookGroupPoster.js
+++ handlers/facebookGroupPoster.js
@@ -5,7 +5,7 @@ const dayjs = require('dayjs')
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 const month = monthNames[new Date().getMonth()].toLowerCase()
 const day = dayjs().date().toString()
-const link = `http://173.212.239.101/img/spb-calendar-fb/${month}/${day}.png`
+const link = `http://173.212.239.101/img/spb-calendar-fb/${month}/${day}.jpg`
 const url = `https://graph.facebook.com/v13.0/600790771639073/feed?access_token=${process.env.FB_ACCESS_TOKEN_KV_GROUPS}&link=${link}`
 
 async function facebookGroupPoster() {
