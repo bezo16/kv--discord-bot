58a4d0f4149c7faabaf64d24f312546fba82fa30
bezo16 <jakub@buff.game>
2022-08-27 11:59:17 +0200

    fixed from static to dynamic fb group posting

--- functions/facebookGroupPoster.js
+++ functions/facebookGroupPoster.js
@@ -3,11 +3,11 @@ const axios = require('axios')
 const dayjs = require('dayjs')
 
 const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-const month = monthNames[new Date().getMonth()].toLowerCase()
-const day = dayjs().date().toString()
-const link = `http://173.212.239.101/img/spb-calendar-fb/${month}/${day}.jpg`
 
 async function facebookGroupPoster(groupId) {
+  const month = monthNames[new Date().getMonth()].toLowerCase()
+  const day = dayjs().date().toString()
+  const link = `http://173.212.239.101/img/spb-calendar-fb/${month}/${day}.jpg`
   const url = `https://graph.facebook.com/v13.0/${groupId}/feed?access_token=${process.env.FB_ACCESS_TOKEN_KV_GROUPS}&link=${link}`
   try {
     await axios.post(url)
