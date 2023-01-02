bd5ce695c9d6ba44aeb19473c73c50b79f6fe272
bezo16 <jakub@buff.game>
2023-01-02 20:15:08 +0100

    fix: pupeeter sandbox error launch

--- functions/scraping/puppeteerFetch.ts
+++ functions/scraping/puppeteerFetch.ts
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer")
 
 
 const puppeteerFetch = async (url : string) => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ args: ["--no-sandbox", "--disable-setuid-sandbox"] })
   const page = await browser.newPage()
   await page.goto(url)
   const data = await page.content() as string
