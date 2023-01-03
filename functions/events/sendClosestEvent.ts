b459257bca42c8f584d0e09842a093742c5d3e9a
bezo16 <jakub@buff.game>
2023-01-03 09:29:26 +0100

    refactor: sendClosestEvent

--- functions/events/sendClosestEvent.ts
+++ functions/events/sendClosestEvent.ts
@@ -1,33 +1,13 @@
-import Cheerio from "cheerio"
-import puppeteerFetch from "../scraping/puppeteerFetch"
 import { Client, TextChannel, EmbedBuilder } from "discord.js"
 import dayjs from "dayjs"
 import monthToNumber from "../../utils/date/monthToNumber"
-
-type event = {
-    day: string
-    month: string
-    link: string
-    title: string
-    desc: string
-}
+import yogapitEvents from "../scraping/yogapitEvents"
 
 const sendClosestEvent = async (client: Client, channelId: string) => {
   const channel = client.channels.cache.get(channelId) as TextChannel
-  const data = await puppeteerFetch("https://yogapit.sk/udalosti/")
-  const cheerio = Cheerio.load(data)
+  const { events, isTomorrowEvent } = await yogapitEvents()
+  console.log(isTomorrowEvent)
 
-  const events: event[] = []
-  cheerio(".owl-item").each((i, e) => {
-    const day = cheerio(".pi-day", e).text()
-    const month = cheerio(".pi-month", e).text()
-    const title = cheerio(".pi-title", e).text()
-    const desc = cheerio(".pi-desc", e).text()
-    const link = cheerio(".item-top a", e).attr("href") as string
-    events.push({ day, month, link, title, desc })
-  }) // create events array
-
-  const closestDate = ""
   for (const event of events) {
     const currentYear = dayjs().year()
     const isAfter = dayjs().isBefore(dayjs(`${currentYear}-${monthToNumber(event.month.padStart(2, "0"))}-${event.day.padStart(2, "0")}T10:00:00`))
---
+++ functions/scraping/yogapitEvents.ts
@@ -0,0 +1,46 @@
+import Cheerio from "cheerio"
+import puppeteerFetch from "../scraping/puppeteerFetch"
+import dayjs = require("dayjs")
+import monthToNumber from "../../utils/date/monthToNumber"
+
+type event = {
+    day: string
+    month: string
+    link: string
+    title: string
+    desc: string
+}
+
+
+const yogapitEvents = async () => {
+
+  // fetch data
+  const data = await puppeteerFetch("https://yogapit.sk/udalosti/")
+  const cheerio = Cheerio.load(data)
+
+  // create events array
+  const events: event[] = []
+  cheerio(".owl-item").each((i, e) => {
+    const day = cheerio(".pi-day", e).text()
+    const month = cheerio(".pi-month", e).text()
+    const title = cheerio(".pi-title", e).text()
+    const desc = cheerio(".pi-desc", e).text()
+    const link = cheerio(".item-top a", e).attr("href") as string
+    events.push({ day, month, link, title, desc })
+  })
+
+  // is tomorow event
+  let isTomorrowEvent = false
+  for (const event of events) {
+    const currentYear = dayjs().year()
+    const dayDifference = dayjs(`${currentYear}-${monthToNumber(event.month.padStart(2, "0"))}-${event.day.padStart(2, "0")}T10:00:00`).diff(dayjs(), "days")
+    if (dayDifference === 1) {
+      isTomorrowEvent = true
+      break
+    }
+  }
+
+  return { events, isTomorrowEvent }
+}
+
+export default yogapitEvents
