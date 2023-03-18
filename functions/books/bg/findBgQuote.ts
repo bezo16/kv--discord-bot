be2ceb42dd3a7eb01fd27cb35cd7d51c94e62090
bezo16 <jakub@buff.game>
2023-03-18 13:19:03 +0100

    build: find BG quote util function

---
+++ functions/books/bg/findBgQuote.ts
@@ -0,0 +1,42 @@
+import { Message } from "discord.js"
+import bg from "../../../data/bg/BG-cs"
+
+const findBgQuote = (quoteString: string, message: Message) => {
+  if (!quoteString || !message) return null
+
+  const chapterNum = Number(quoteString.split(".")[0])
+  const quoteNum = Number(quoteString.split(".")[0])
+
+  if (!quoteString.match(/^\d+\.\d+$/g)) {
+    message.channel.send("wrong regex")
+
+    return
+  }
+
+  if (chapterNum < 1 || chapterNum > 18) {
+    message.channel.send("wrong chapter number")
+
+    return null
+  }
+
+  if (quoteNum < 1 || quoteNum > Number(bg[chapterNum - 1][bg[chapterNum - 1].length - 1].number)) {
+    message.channel.send("wrong quote number")
+
+    return null
+  }
+
+
+  const quoteIndex = bg[chapterNum - 1].findIndex(q => {
+    if (q.number === String(quoteNum)) return true
+    if (q.number.includes("-")) {
+      const firstNum = q.number.split("-")[0]
+      const secondNum = q.number.split("-")[1]
+      if (quoteNum >= Number(firstNum) && quoteNum <= Number(secondNum)) return true
+    }
+  })
+  const resultQuote = bg[chapterNum - 1][quoteIndex]
+
+  return resultQuote
+}
+
+export default findBgQuote
