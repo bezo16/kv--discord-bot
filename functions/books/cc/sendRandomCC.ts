641c00550f33b44cf77c75b94e5ed89b127e2973
bezo16 <jakub@buff.game>
2023-03-19 08:56:13 +0100

    refactor: sendRandomCC

--- functions/books/cc/sendRandomCC.ts
+++ functions/books/cc/sendRandomCC.ts
@@ -4,11 +4,9 @@ import { Message } from "discord.js"
 function sendRandomCC(message: Message) {
 
   const cantoNum = Math.floor(Math.random() * 3)
-  const canto = cc[cantoNum]
-  const chapterNum = Math.floor(Math.random() * canto.length)
-  const chapter = canto[chapterNum]
-  const quoteNum = Math.floor(Math.random() * chapter.length)
-  message.channel.send({ content: `${chapter[quoteNum]} ** ${cantoNum + 1}.${chapterNum + 1}.${quoteNum + 1} ** ` })
+  const chapterNum = Math.floor(Math.random() * cc[cantoNum].length)
+  const quoteNum = Math.floor(Math.random() * cc[cantoNum][chapterNum].length)
+  message.channel.send({ content: `${cc[cantoNum][chapterNum][quoteNum]} ** ${cantoNum + 1}.${chapterNum + 1}.${quoteNum + 1} ** ` })
 }
 
 export default sendRandomCC
