841a25545a1d092fe374fd286ce2bd2840ac823e
bezo16 <jakub@buff.game>
2023-05-11 17:26:27 +0200

    refactor: ?cc r

--- functions/books/cc/sendRandomCC.ts
+++ functions/books/cc/sendRandomCC.ts
@@ -1,12 +1,16 @@
-import cc from "../../../data/cc/cc"
+import cc from "../../../data/cc/caitanya-caritamrta"
 import { Message } from "discord.js"
+import findCcQuote from "./findCcQuote"
+import createTextEmbed from "../../common/createTextEmbed"
 
 function sendRandomCC(message: Message) {
 
   const cantoNum = Math.floor(Math.random() * 3)
   const chapterNum = Math.floor(Math.random() * cc[cantoNum].length)
   const quoteNum = Math.floor(Math.random() * cc[cantoNum][chapterNum].length)
-  message.channel.send({ content: `${cc[cantoNum][chapterNum][quoteNum]} ** ${cantoNum + 1}.${chapterNum + 1}.${quoteNum + 1} ** ` })
+  const resultQuote = findCcQuote(`${cantoNum + 1}.${chapterNum + 1}.${quoteNum + 1}`, message)
+  const embed = createTextEmbed({ description: `${resultQuote!.text} \n\n [${resultQuote?.bookName} ${resultQuote?.chapter}.${resultQuote!.number}](https://vedabase.io${resultQuote!.link})`, title: "Hare Krišna" })
+  message.channel.send({ embeds: [embed] })
 }
 
 export default sendRandomCC
