abfa48d5e18c4c0a5cf953ab457b8ad35d466044
bezo16 <jakub@buff.game>
2023-05-11 17:34:39 +0200

    refactor: ?cci r

--- functions/books/cc/sendRandomCCImage.ts
+++ functions/books/cc/sendRandomCCImage.ts
@@ -1,6 +1,7 @@
 import cc from "../../../data/cc/cc"
 import sendImg from "../../canvas/sendImageQuote"
 import { Message } from "discord.js"
+import findCcQuote from "./findCcQuote"
 
 function sendRandomCCImage(message: Message) {
   const cantoNum = Math.floor(Math.random() * 3)
@@ -8,7 +9,8 @@ function sendRandomCCImage(message: Message) {
   const chapterNum = Math.floor(Math.random() * cantoArray.length)
   const chapter = cantoArray[chapterNum]
   const quoteNum = Math.floor(Math.random() * chapter.length)
-  sendImg(message, chapter[quoteNum], `Śrī Caitanya-Caritāmrta ${cantoNum + 1}.${chapterNum + 1}.${quoteNum + 1}`)
+  const resultQuote = findCcQuote(`${cantoNum + 1}.${chapterNum + 1}.${quoteNum + 1}`, message)
+  sendImg(message, resultQuote!.text, `${resultQuote?.bookName} ${resultQuote?.chapter}.${resultQuote!.number}`)
 }
 
 export default sendRandomCCImage
