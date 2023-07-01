3332c5e2bdb7979feb3ad1a147a01dba62976640
bezo16 <jakub@buff.game>
2023-07-01 12:34:14 +0200

    ?bgi r fix

--- functions/books/bg/sendRandomBgImage.ts
+++ functions/books/bg/sendRandomBgImage.ts
@@ -7,7 +7,7 @@ import findBgQuote from "./findBgQuote"
 function sendRandomBgImage(message: Message) {
   const chapterNum = Math.floor(Math.random() * 18)
   const quoteNum = Math.floor(Math.random() * bg[chapterNum].length)
-  const resultquote = findBgQuote(`${chapterNum}.${quoteNum}`, "sk", message)
+  const resultquote = findBgQuote(`${chapterNum + 1}.${quoteNum + 1}`, "sk", message)
 
   if (!resultquote) {
     message.channel.send("quote not found")
