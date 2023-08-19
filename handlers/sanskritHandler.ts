9a2f8dac1d0bb8fa5697ec15f644bb017a5be0ce
bezo <jakub@buff.game>
2023-08-19 10:45:05 +0200

    feat: searching sanskrit words

--- handlers/sanskritHandler.ts
+++ handlers/sanskritHandler.ts
@@ -1,19 +1,47 @@
 import { Message } from "discord.js"
 import sanskrit from "../data/other/sanskrit"
 import createTextEmbed from "../functions/common/createTextEmbed"
+import MiniSearch from "minisearch"
 
 
-function sanskritHandler(message: Message) {
+async function sanskritHandler(message: Message) {
   const words = message.content.split(" ")
-  if (words.length !== 2) return
+  if (words.length < 2) return
   const [firstWord, secondWord] = words
   if (!["?sanskrit"].includes(firstWord)) return
   const sanskritWords = Object.keys(sanskrit)
 
   if (firstWord === "?sanskrit" && secondWord === "r") {
     const sanskritIndex = Math.floor(Math.random() * sanskritWords.length)
-    const embed = createTextEmbed({description: (sanskrit as any)[sanskritWords[sanskritIndex]], title: sanskritWords[sanskritIndex]})
+    const embed = createTextEmbed({ description: (sanskrit as any)[sanskritWords[sanskritIndex]], title: sanskritWords[sanskritIndex] })
     message.channel.send({ embeds: [embed] })
+  }
+
+  else {
+    const searchableArray: any = []
+    sanskritWords.forEach(word => {
+      if (!word) return
+      searchableArray.push({ word, translation: (sanskrit as any)[word] })
+    })
+    const miniSearch = new MiniSearch({
+      fields: ["word"],
+      storeFields: ["word", "translation"],
+      idField: "word"
+    })
+
+    miniSearch.addAll(searchableArray)
+
+    const results = miniSearch.search(message.content.split(" ").slice(1).join(" "))
+
+    if (results.length === 0) {
+      await message.reply({ content: "sanskrit not found" })
+
+      return
+    }
+
+    const result = results[0]
+    const embed = createTextEmbed({ description: result.translation, title: result.word })
+    await message.reply({ embeds: [embed] })
   }
 }
 
