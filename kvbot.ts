d5571b8fdca7ac38eb4c9631e49dd129b27ac16a
bezo <jakub@buff.game>
2023-12-02 12:45:30 +0100

    redactor: modalSubmissionHandler

--- kvbot.ts
+++ kvbot.ts
@@ -22,6 +22,7 @@ import createEvent from "./functions/slash-commands/create-event/createEvent"
 import findQuoteFunc from "./functions/slash-commands/find-quote/findQuoteFunc"
 import sanskritHandler from "./handlers/sanskritHandler"
 import langChainHandler from "./handlers/langChainHandler"
+import modalSubmissionHandler from "./handlers/interactions/modalSubmissionHandler"
 
 
 config()
@@ -42,31 +43,13 @@ client.once("ready", () => {
   intervalsHandler(client)
 })
 
-// Slash Commands
-client.on("interactionCreate", (interaction) => {
-  if (interaction.isChatInputCommand()) slashCommandsHandler(interaction)
-})
-
-// Modal Submitions
+// Interactions
 client.on("interactionCreate", async interaction => {
-  if (!interaction.isModalSubmit()) return
-
-  if (interaction.customId === "eventCreateModal") {
-    const name = interaction.fields.getTextInputValue("name")
-    const desc = interaction.fields.getTextInputValue("desc")
-    const date = interaction.fields.getTextInputValue("date")
-    createEvent(client, interaction.channelId as string, { name, desc, date } )
-    await interaction.reply({ content: "uspešne si vytvoril událosť", ephemeral: true })
-  }
-
-  if (interaction.customId === "findQuote") {
-    const book = interaction.fields.getTextInputValue("book")
-    const text = interaction.fields.getTextInputValue("text")
-    findQuoteFunc(interaction, book, text)
-  }
+  if (interaction.isChatInputCommand()) slashCommandsHandler(interaction)
+  if (interaction.isModalSubmit()) modalSubmissionHandler(interaction, client)
 })
 
-// Message Created
+// Messages
 client.on("messageCreate", (message) => {
   if (message.author.bot) return
   message.content = message.content.toLowerCase()
