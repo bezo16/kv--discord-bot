a4104c1a7271c33f252ae23a596a332e30108f12
bezo <jakub@buff.game>
2023-12-15 12:29:30 +0100

    fix

--- functions/slash-commands/send-sp-quote/sendSpQuoteFunc.ts
+++ functions/slash-commands/send-sp-quote/sendSpQuoteFunc.ts
@@ -1,10 +1,11 @@
 // @ts-nocheck
 import { CacheType, ChatInputCommandInteraction } from "discord.js";
 import spDailyQuotesLinks from "../../../data/images/sp_daily_quotes_links";
+import spSBQuotesLinks from "../../../data/images/sp_photos_srimad_bhagavatam_links";
 import spBGQuotesLinks from "../../../data/images/sp_photos_srimad_bhagavad_gita_links";
 
 const sendSpQuoteFunc = async(interaction: ChatInputCommandInteraction<CacheType>) => {
-    const category = interaction.options.getString("category") as "daily" | "sb"
+    const category = interaction.options.getString("category") as "daily" | "sb" | "bg"
     
     let imageUrl = ""
     if (category === "daily") imageUrl = spDailyQuotesLinks[Math.floor(Math.random() * spDailyQuotesLinks.length)]
